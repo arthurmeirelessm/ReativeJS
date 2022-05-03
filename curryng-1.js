bb1af76ccf4fec856d30083417ab3161874c3f3d
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-05-03 14:52:01 -0300

    Starting Lazy Valuation

--- curryng-1.js
+++ curryng-1.js
@@ -1,15 +1,34 @@
-function textoComTamanhoEntre(min, max, erro, texto) {
-    const tamanho = texto
-    if (tamanho < min || tamanho > max) {
-        throw erro
-    }   
+function textoComTamanhoEntre(min) {
+    return function (max) {
+        return function (erro) {
+            return function (texto) {
+                const tamanho = texto
+                if (tamanho < min || tamanho > max) {
+                    throw erro
+                }
 
-    return tamanho.trim()
+                return tamanho.trim()
+            }
+        }
+    }
 }
 
 
 
-const p1 = {nome: 'Art vfv fgg', preco: 14.99, desc: 0.25}
+function applicationValidacao(fn, valor) {
+    try {
+        fn(valor)
+    } catch (e) {
+        return { erro: e }
+    }
+}
 
 
-console.log(textoComTamanhoEntre(4, 15, 'Erro!', p1.nome))
+
+
+const forcarTamanhoPadrao = textoComTamanhoEntre(4)(255)
+const forcarNomeProdutoValido = forcarTamanhoPadrao('Nome produto inválido!')
+
+const p1 = { nome: 'Art vfv fgg', preco: 14.99, desc: 0.25 }
+
+console.log(forcarNomeProdutoValido(p1.nome))
