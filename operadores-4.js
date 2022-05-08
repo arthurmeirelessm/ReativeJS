174ce74b064637643b58d0469a13a1372cd74ce4
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-05-08 03:04:25 -0300

    Operadores-4

---
+++ operadores-4.js
@@ -0,0 +1,57 @@
+const { from, Observable, observable, pipe } = require('rxjs')
+
+
+function primeiro() {
+    return function (source) {
+        return new Observable(subscriber => {
+            source.subscribe({
+                next(v) {
+                    subscriber.next(1)
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+
+function ultimo() {
+    return function (source) {
+        return new Observable(subscriber => {
+            let ultimo
+            source.subscribe({
+                next(v) {
+                    ultimo = v
+                },
+                complete() {
+                    if (ultimo !== undefined) {
+                        subscriber.next(ultimo)
+                    }
+                    subscriber.complete()
+                }
+
+            })
+        })
+    }
+}
+
+
+function nenhum() {
+    return function (source) {
+        return new Observable(subscriber => {
+            source.subscribe({
+                next(v) {
+
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+
+
+from([2, 5, 3, 4, 6, 5, 6])
+   // .pipe(nenhum())
+    .pipe(ultimo())
+    .subscribe(console.log)
